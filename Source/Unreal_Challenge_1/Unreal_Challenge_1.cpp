2bc61d4d69cb2ce0d92c73478d8be71aa42cfaa8
Zion Obcena <zmobcena@gmail.com>
2024-10-18 21:16:02 +0800

    Initial commit

---
+++ Source/Unreal_Challenge_1/Unreal_Challenge_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Unreal_Challenge_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_Challenge_1, "Unreal_Challenge_1" );
